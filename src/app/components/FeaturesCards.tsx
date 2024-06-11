a80604c60e33608f399e99cdde26c117d1891c65
Juanies <juanromeroviudez@gmail.com>
2024-06-11 14:39:42 +0200

    Arrow responsive with cards

--- src/app/components/FeaturesCards.tsx
+++ src/app/components/FeaturesCards.tsx
@@ -2,9 +2,9 @@ export default function FeaturesCards(){
   const features: string[] = ["Talleres", "Comunidad Activa", "Busca ayuda"];
 
   return(
-     <div className="flex  w-[100%] justify-between items-center min-h-[60dvh]">
+     <div className="flex  w-[100%] justify-between lg:flex-row flex-col gap-8 items-center">
         {features.map((feature, index) => (
-          <div className="border text-[#939393] font-semibold border-[#939393] rounded-xl text-[2rem] px-10 py-4" key={index}>
+          <div className="border  text-[#939393] font-semibold border-[#939393] rounded-xl text-[2rem] px-10 py-4" key={index}>
             {feature}
           </div>
         ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
+"use client"
+import { useEffect, useState } from 'react';
 import Image from "next/image";
-import Proton from "../../images/tecno.png"
+import Proton from "../../images/tecno.png";
 import HyperLink from "./components/HyperLink";
 import Arrow from "../../images/Arrow";
 import FeaturesCards from "./components/FeaturesCards";
+
 export default function Home() {
+
+
+
   return (
     <main className="flex min-h-screen mx-auto container mt-14 flex-col lg:max-w-6xl md:max-w-2xl items-center justify-between ">
-      <section className="flex  w-[100%] justify-between items-center min-h-[60dvh]" >
+      <section className="flex   w-[100%] justify-between items-center min-h-[60dvh] relative" >
         <div>
           <h1 className=" text-[##939393] leading-normal text-[4rem] max-w-[8em] font-semibold">
             Programadores y <span className="text-[#0070B9]">estudiantes</span>
           </h1>
           <p className="font-semibold text-[#939393] text-[2rem]">Piensa, codifica, revoluciona</p>
-          <HyperLink text="Unete ya" url="#"/>
+          <HyperLink text="Unete ya" url='./page.tsx'   />
+          <Arrow className="absolute lg:block hidden  left-[10rem] top-[23rem]" />
         </div>
         <Image src={Proton} alt="Mujer aprendiendo." width="420" height="420" />
       </section>
-      <Arrow className="absolute left-[25em] top-[30em]"/>
       <FeaturesCards/>
     </main>
   );
