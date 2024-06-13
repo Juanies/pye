ba2d62f44343ae7d051f2b1ea451cc77ba32301b
Juanies <juanromeroviudez@gmail.com>
2024-06-13 21:14:39 +0200

    Login with Discord

---
+++ src/app/components/LoginForm.tsx
@@ -0,0 +1,14 @@
+import { doSocialLogin, doLogout } from "@/app/utils/action"
+
+const LoginForm = () => {
+  return (
+    <form action={doSocialLogin}>
+        <button  type="submit" name="action" value="discord">
+          Sign in with Discord
+        </button>
+      
+    </form>
+  )
+}
+
+export default LoginForm
---
+++ src/app/components/Offers.tsx
@@ -0,0 +1,13 @@
+export default function Offers() {
+  return (
+    <div className="">
+      <h2 className="">Ofertas de empleo</h2>
+      <div className="">
+        <div>
+          {/* Aquí se colocará la imagen */}
+          {/* img */}
+        </div>
+      </div>
+    </div>
+  );
+}
