2fcab599ecb92d4d9fc9e5a188414b79103cd2e5
Juanies <juanromeroviudez@gmail.com>
2024-07-01 22:23:16 +0200

    Refator Features cards

--- components/elements/FeaturesCards.tsx
+++ components/elements/FeaturesCards.tsx
@@ -9,14 +9,15 @@ export default function FeaturesCards(){
     "Busca ayuda": Search
   };
 
+  const featureList = Object.entries(featureObje).map(([featureName, FeatureIcon], index) => (
+    <div className="border text-[#939393] flex items-center justify-center flex-col  font-semibold bg-gradient-to-tl from-[#EAE8E8] via-[#EAE8E8] to-sky-200 to-95% rounded-xl text-[2rem] px-10 py-4" key={index}>
+      <FeatureIcon/>
+      {featureName}
+    </div>
+  ))
   return (
     <div className="flex w-[100%] justify-between lg:flex-row flex-col gap-8 items-center">
-      {Object.entries(featureObje).map(([featureName, FeatureIcon], index) => (
-        <div className="border text-[#939393] flex items-center justify-center flex-col  font-semibold bg-gradient-to-tl from-[#EAE8E8] via-[#EAE8E8] to-sky-200 to-95% rounded-xl text-[2rem] px-10 py-4" key={index}>
-          <FeatureIcon/>
-          {featureName}
-        </div>
-      ))}
+      {featureList}
     </div>
   );
 }
